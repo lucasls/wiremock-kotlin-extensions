2cf722fe035c8334b0d6f768017db2f585796ef2
Lucas Laurindo dos Santos <lucas.ls@mytaxi.com>
2020-07-02 18:56:47 +0200

    Adding missing Wiremock Extensions

--- src/main/kotlin/com/lucasls/kotlinx/wiremock/WireMockExtensions.kt
+++ src/main/kotlin/com/lucasls/kotlinx/wiremock/WireMockExtensions.kt
@@ -1,3 +1,4 @@
+@file:Suppress("unused")
 package com.lucasls.kotlinx.wiremock
 
 import com.github.tomakehurst.wiremock.client.CountMatchingStrategy
@@ -7,10 +8,15 @@ import com.github.tomakehurst.wiremock.client.WireMock
 import com.github.tomakehurst.wiremock.client.WireMockBuilder
 import com.github.tomakehurst.wiremock.extension.Parameters
 import com.github.tomakehurst.wiremock.http.Request
+import com.github.tomakehurst.wiremock.matching.RequestPatternBuilder
 import com.github.tomakehurst.wiremock.matching.UrlPathPattern
 import com.github.tomakehurst.wiremock.matching.UrlPattern
 import com.github.tomakehurst.wiremock.matching.ValueMatcher
+import com.github.tomakehurst.wiremock.recording.RecordSpecBuilder
+import com.github.tomakehurst.wiremock.stubbing.ServeEvent
+import com.github.tomakehurst.wiremock.stubbing.StubImportBuilder
 import com.github.tomakehurst.wiremock.stubbing.StubMapping
+import com.github.tomakehurst.wiremock.verification.LoggedRequest
 
 inline fun createWireMock(block: WireMockBuilder.() -> Unit = {}): WireMock =
     WireMock.create()
@@ -49,6 +55,8 @@ fun moreThanOrExactly(expected: Int): CountMatchingStrategy = WireMock.moreThanO
 
 fun moreThan(expected: Int): CountMatchingStrategy = WireMock.moreThan(expected)
 
+inline fun get(urlPattern: UrlPattern, block: MappingBuilder.() -> Unit = {}): MappingBuilder =
+    WireMock.get(urlPattern).also(block)
 
 inline fun post(urlPattern: UrlPattern, block: MappingBuilder.() -> Unit = {}): MappingBuilder =
     WireMock.post(urlPattern).also(block)
@@ -168,3 +176,62 @@ inline fun serviceUnavailable(block: ResponseDefinitionBuilder.() -> Unit = {}):
 
 inline fun status(status: Int, block: ResponseDefinitionBuilder.() -> Unit = {}): ResponseDefinitionBuilder =
     WireMock.status(status).also(block)
+
+inline fun verify(block: () -> RequestPatternBuilder) =
+    WireMock.verify(block())
+
+inline fun verify(count: Int, block: () -> RequestPatternBuilder) =
+    WireMock.verify(count, block())
+
+inline fun verify(
+    countMatchingStrategy: CountMatchingStrategy?,
+    block: () -> RequestPatternBuilder
+) = WireMock.verify(countMatchingStrategy, block())
+
+inline fun findAll(block: () -> RequestPatternBuilder): List<LoggedRequest> =
+    WireMock.findAll(block())
+
+inline fun removeServeEvents(block: () -> RequestPatternBuilder): List<ServeEvent> =
+    WireMock.removeServeEvents(block())
+
+inline fun getRequestedFor(urlPattern: UrlPattern, block: RequestPatternBuilder.() -> Unit): RequestPatternBuilder =
+    WireMock.getRequestedFor(urlPattern).apply(block)
+
+inline fun postRequestedFor(urlPattern: UrlPattern, block: RequestPatternBuilder.() -> Unit): RequestPatternBuilder =
+    WireMock.postRequestedFor(urlPattern).apply(block)
+
+inline fun putRequestedFor(urlPattern: UrlPattern, block: RequestPatternBuilder.() -> Unit): RequestPatternBuilder =
+    WireMock.putRequestedFor(urlPattern).apply(block)
+
+inline fun deleteRequestedFor(urlPattern: UrlPattern, block: RequestPatternBuilder.() -> Unit): RequestPatternBuilder =
+    WireMock.deleteRequestedFor(urlPattern).apply(block)
+
+inline fun patchRequestedFor(urlPattern: UrlPattern, block: RequestPatternBuilder.() -> Unit): RequestPatternBuilder =
+    WireMock.patchRequestedFor(urlPattern).apply(block)
+
+inline fun headRequestedFor(urlPattern: UrlPattern, block: RequestPatternBuilder.() -> Unit): RequestPatternBuilder =
+    WireMock.headRequestedFor(urlPattern).apply(block)
+
+inline fun optionsRequestedFor(urlPattern: UrlPattern, block: RequestPatternBuilder.() -> Unit):
+        RequestPatternBuilder =
+    WireMock.optionsRequestedFor(urlPattern).apply(block)
+
+inline fun traceRequestedFor(urlPattern: UrlPattern, block: RequestPatternBuilder.() -> Unit): RequestPatternBuilder =
+    WireMock.traceRequestedFor(urlPattern).apply(block)
+
+inline fun anyRequestedFor(urlPattern: UrlPattern, block: RequestPatternBuilder.() -> Unit): RequestPatternBuilder =
+    WireMock.anyRequestedFor(urlPattern).apply(block)
+
+inline fun requestMadeFor(
+    customMatcherName: String, parameters: Parameters, block: RequestPatternBuilder.() -> Unit
+): RequestPatternBuilder = WireMock.requestMadeFor(customMatcherName, parameters).apply(block)
+
+inline fun requestMadeFor(
+    requestMatcher: ValueMatcher<Request>, block: RequestPatternBuilder.() -> Unit
+): RequestPatternBuilder = WireMock.requestMadeFor(requestMatcher).apply(block)
+
+inline fun recordSpec(block: RecordSpecBuilder.() -> Unit): RecordSpecBuilder =
+    WireMock.recordSpec().apply(block)
+
+inline fun importStubs(block: () -> StubImportBuilder) =
+    WireMock.importStubs(block())
